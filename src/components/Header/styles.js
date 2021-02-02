3ae9adc90592bfda81c543961324e0f2a59270e2
herickmotta <herickrossato@gmail.com>
2021-02-02 14:10:26 -0300

    feat: header add shadow box/height adjust

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,23 +3,25 @@ import Tipografy from "../../config/typography";
 
 export const Container = styled.div`
   width: 100vw;
-  height: 90px;
-  display:flex;
+  height: 80px;
+  display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 20px 40px;
-  img{
+  padding: 15px 20px;
+  box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.15);
+  z-index: 1;
+  img {
     height: auto;
-    cursor:pointer;
+    cursor: pointer;
   }
 `;
 
 export const LeftBox = styled.div`
   display: flex;
   font-size: 18px;
-`
+`;
 export const LogoBox = styled.div`
-  img{
+  img {
     max-height: 40px;
   }
 `;
@@ -30,18 +32,17 @@ export const Nav = styled.nav`
   align-items: center;
   justify-content: space-between;
   margin: 0 20px;
-  a{
+  a {
     padding: 10px;
   }
-  a:hover{
+  a:hover {
     opacity: 50%;
   }
 `;
 export const AvatarBox = styled.div`
   height: 100%;
-  img{
+  img {
     border-radius: 50%;
     height: 100%;
   }
 `;
-
--- src/pages/Course/styles.js
+++ src/pages/Course/styles.js
@@ -5,6 +5,7 @@ export const Container = styled.body`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.25);
 `;
 
 export const temp = 0;
