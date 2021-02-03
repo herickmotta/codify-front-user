69d161d079e5cff9f0c4701513d29aafaab0b363
LNakandakare <lucasnakaan@gmail.com>
2021-02-03 17:13:03 -0300

    refactor: save login user data in local storage

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,11 +1,12 @@
 import React, { useState, createContext } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const UserContext = createContext();
 
 export default UserContext;
 
 export function UserProvider({ children }) {
-  const [user, setUser] = useState();
+  const [user, setUser] = useLocalStorage("user", null);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
