807cda906b78a9e23d1332c8fb352ee38ced5b31
herickmotta <herickrossato@gmail.com>
2021-02-03 17:50:41 -0300

    refactor: change static props to mocked ones

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,15 +1,12 @@
 import React, { useState, createContext } from "react";
+import mockedUser from "../mock/mockedUser";
 
 const UserContext = createContext();
 
 export default UserContext;
 
 export function UserProvider({ children }) {
-  const [user, setUser] = useState({
-    name: "Usuário",
-    photo:
-      "https://i1.wp.com/terracoeconomico.com.br/wp-content/uploads/2019/01/default-user-image.png?ssl=1",
-  });
+  const [user, setUser] = useState(mockedUser());
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
--- src/pages/Home/components/CourseSnippet/index.js
+++ src/pages/Home/components/CourseSnippet/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import Button from "../../../../components/Button";
 import { Snippet, ImageBox, DescriptionBox, ButtonBox } from "./styles";
 
-export default function CourseSnippet() {
+export default function CourseSnippet({ course }) {
+  const { name, description, photo } = course;
   return (
     <Snippet>
       <ImageBox>
-        <img src="/images/courseimg.png" alt="" />
+        <img src={photo} alt="" />
       </ImageBox>
 
       <DescriptionBox>
-        <h1>JavaScript do zero!</h1>
-        <p> Aprenda JavaScript do zero ao avançado, com muita prática!</p>
+        <h1>{name}</h1>
+        <p> {description}</p>
         <div>Ver mais</div>
       </DescriptionBox>
 
