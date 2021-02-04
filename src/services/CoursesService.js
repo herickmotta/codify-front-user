08471f8de5184be1cd7107eec89e5f860d423b6c
thiagoribb <thiribeiro142@gmail.com>
2021-02-04 20:06:51 -0300

    feat: get course by id - CoursesService

---
+++ src/services/CoursesService.js
@@ -0,0 +1,34 @@
+/* eslint-disable class-methods-use-this */
+import api from "../config/api";
+
+class CoursesService {
+  async getAll(token) {
+    try {
+      const { data } = await api.get(`/courses/`, {
+        headers: { Authorization: `JWT ${token}` },
+      });
+      if (data) {
+        return { success: data };
+      }
+      return null;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getById(id, token) {
+    try {
+      const { data } = await api.get(`/courses/${id}`, {
+        headers: { Authorization: `JWT ${token}` },
+      });
+      if (data) {
+        return { success: data };
+      }
+      return null;
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+export default new CoursesService();
