5b9948a2ec6f40148127ba64b49a85b0b7657999
thiagoribb <thiribeiro142@gmail.com>
2021-02-09 20:53:21 -0300

    feat: api integration accordion chapters

--- src/pages/Course/components/AccordionChapters/index.js
+++ src/pages/Course/components/AccordionChapters/index.js
@@ -7,40 +7,23 @@ import {
   AccordionItemButton,
   AccordionItemPanel,
 } from "react-accessible-accordion";
-
 import "react-accessible-accordion/dist/fancy-example.css";
 
-export default function AccordionChapters() {
+export default function AccordionChapters({ chapters }) {
   return (
-    <Accordion>
-      <AccordionItem>
-        <AccordionItemHeading>
-          <AccordionItemButton>
-            What harsh truths do you prefer to ignore?
-          </AccordionItemButton>
-        </AccordionItemHeading>
-        <AccordionItemPanel>
-          <p>
-            Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
-            occaecat ut occaecat consequat est minim minim esse tempor laborum
-            consequat esse adipisicing eu reprehenderit enim.
-          </p>
-        </AccordionItemPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <AccordionItemHeading>
-          <AccordionItemButton>
-            Is free will real or just an illusion?
-          </AccordionItemButton>
-        </AccordionItemHeading>
-        <AccordionItemPanel>
-          <p>
-            In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-            nostrud velit in irure cillum tempor laboris sed adipisicing eu esse
-            duis nulla non.
-          </p>
-        </AccordionItemPanel>
-      </AccordionItem>
+    <Accordion className="accordion">
+      {chapters.map((chapter) => (
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>{chapter.name}</AccordionItemButton>
+          </AccordionItemHeading>
+          {chapter.topics.map((topic) => (
+            <AccordionItemPanel>
+              <p>{topic.name}</p>
+            </AccordionItemPanel>
+          ))}
+        </AccordionItem>
+      ))}
     </Accordion>
   );
 }
--- src/pages/Course/styles.js
+++ src/pages/Course/styles.js
@@ -8,90 +8,9 @@ export const Container = styled.body`
   justify-content: space-between;
   align-items: center;
   font-family: ${tipography.Roboto};
-`;
 
-export const CointainerCourseText = styled.div`
-  width: 100%;
-  height: 200px;
-  background-image: linear-gradient(to bottom, #e4d04d, #f2e799);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding-top: 20px;
-  padding-bottom: 80px;
-  z-index: 0;
-  position: relative;
-  margin-bottom: 100px;
-
-  & > h1 {
-    margin: 20px 0;
-    font-size: 25px;
-    font-weight: bold;
-  }
-  & > p {
-    color: #383838;
-  }
-`;
-
-export const ContainerUserProgress = styled.div`
-  width: 85%;
-  height: 100px;
-  display: flex;
-  background: #fff;
-  justify-content: space-between;
-  align-items: center;
-  box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.25);
-  font-family: ${tipography.Roboto};
-  border-radius: 5px;
-  padding-top: 15px;
-  padding-bottom: 15px;
-  position: absolute;
-  bottom: -50px;
-  z-index: 1;
-
-  button {
-    width: 160px;
-    margin: auto 20px;
-    font-size: 15px;
-  }
-`;
-
-export const ContainerImgAndProgress = styled.div`
-  display: flex;
-  justify-content: start;
-  align-items: center;
-  width: 50%;
-  margin-left: 15px;
-
-  img {
-    border-radius: 50%;
-    height: 50px;
-    width: 50px;
-  }
-
-  p {
-    margin-bottom: 10px;
-    font-size: 15px;
-  }
-
-  > div {
-    margin-left: 15px;
-  }
-`;
-
-export const ProgressBar = styled.div`
-  height: 20px;
-  margin: 10px;
-  background: #eaeaea;
-  padding: 0px;
-
-  > div {
-    width: 50%;
-    background: #76df92;
-    height: 100%;
-    display: flex;
-    align-items: center;
+  .accordion {
+    width: 80%;
   }
 `;
 
