9d75121db2e88c87ecc621bebeafcfd67809e894
gpbPiazza <pedro.piazza99@gmail.com>
2021-02-11 23:55:19 -0300

    feat: add navigation to study area

--- src/pages/Home/components/CardsSection/index.js
+++ src/pages/Home/components/CardsSection/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import CourseCard from "../CourseCard";
 import { Container, CardsList } from "./styles";
 
--- src/pages/Home/components/CourseCard/index.js
+++ src/pages/Home/components/CourseCard/index.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
+import UserContext from "../../../../contexts/UserContext";
+import CoursesService from "../../../../services/CoursesService";
+
 import { Card, ImageBox, DescriptionBox } from "./styles";
 
 export default function CourseCard({ id, name, description, photo }) {
   const history = useHistory();
+
+  const { user } = useContext(UserContext);
+
+  const goStudyArea = async (courseId) => {
+    const data = await CoursesService.getById(courseId, user.token);
+    if (data) {
+      console.log(data);
+      history.push(`/courses/:id/chapters/:chapterId/topics/:topicId`);
+    } else {
+      alert("Não foi possível navegar para sua área te estudo");
+    }
+  };
   return (
-    <Card onClick={() => history.push(`/courses/${id}`)}>
+    <Card onClick={() => goStudyArea(id)}>
       <ImageBox>
         <img src={photo} alt="" />
       </ImageBox>
