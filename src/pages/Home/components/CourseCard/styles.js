b50c67d39bb1e0f35d712a4865d0e0ab07649212
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2021-02-18 17:01:27 -0300

    Revert "Revert "Refactor/layout""

--- src/pages/Home/components/CourseCard/styles.js
+++ src/pages/Home/components/CourseCard/styles.js
@@ -3,42 +3,60 @@ import colors from "../../../../config/colors";
 import tipography from "../../../../config/typography";
 
 export const Card = styled.div`
-  height: 280px;
-  width: 192px;
+  min-width: 200px;
+  max-height: 500px;
+  min-height: 400px;
+  max-width: 23%;
   border-radius: 20px;
   overflow: hidden;
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: column;
   cursor: pointer;
   box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.1);
   font-family: ${tipography.Roboto};
+  background: ${colors.white};
 `;
 
 export const ImageBox = styled.div`
   height: 50%;
   width: 100%;
-  object-fit: fill;
   display: flex;
   flex-shrink: 0;
   align-items: center;
   justify-content: center;
   img {
-    width: auto;
+    width: 100%;
     height: 100%;
+    object-fit: cover;
   }
 `;
 
-export const StudyButton = styled.div``;
+export const StudyButton = styled.div`
+  width: 100%;
+  font-weight: bold;
+  button {
+    width: 100%;
+    margin: 0;
+    padding: 10px;
+    font-size: 14px;
+    cursor: pointer;
+  }
+`;
 
 export const DescriptionBox = styled.div`
   height: 50%;
   width: 100%;
-  padding: 10px;
+  padding: 20px 10px;
   h1 {
+    font-size: calc(14px + (20 - 14) * ((100vw - 300px) / (1600 - 300)));
     font-weight: bold;
     margin-bottom: 10px;
   }
   p {
+    font-size: calc(14px + (20 - 14) * ((100vw - 300px) / (1600 - 300)));
     color: ${colors.gray};
   }
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
