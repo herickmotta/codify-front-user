d1734087d1dd7eac5433f2b63e599673cb40ad1d
herickmotta <herickrossato@gmail.com>
2021-03-01 17:07:57 -0300

    refactor: header is now position fixed

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,6 +4,8 @@ import MEDIA_MAX from "../../config/media";
 import Tipografy from "../../config/typography";
 
 export const Container = styled.div`
+  position: fixed;
+  top: 0;
   width: 100%;
   height: 80px;
   display: flex;
