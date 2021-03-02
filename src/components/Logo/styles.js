d7ac051a220d1904d11c18ffefe86a296be3c12c
LNakandakare <lucasnakaan@gmail.com>
2021-03-02 14:16:14 -0300

    feat: mobile layout adjust

--- src/components/Logo/styles.js
+++ src/components/Logo/styles.js
@@ -8,6 +8,10 @@ export const Title = styled.h1`
   font-size: 116px;
   letter-spacing: 0.05em;
   line-height: 139px;
+
+  @media (max-width: 500px) {
+    font-size: 111px;
+  }
 `;
 
 export const SubTitle = styled.h2`
@@ -15,8 +19,17 @@ export const SubTitle = styled.h2`
   color: ${colors.white};
   font-size: 34px;
   line-height: 41px;
+
+  @media (max-width: 500px) {
+    font-size: 32px;
+  }
 `;
 
 export const Container = styled.div`
   margin-bottom: 60px;
+
+  @media (max-width: 500px) {
+    width: 100%;
+    margin-bottom: 40px;
+  }
 `;
